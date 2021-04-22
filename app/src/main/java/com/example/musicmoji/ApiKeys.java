745e20043a57d17af18a08a6f8216b5173a768fa
LucianoDMichelli <51410823+LucianoDMichelli@users.noreply.github.com>
2021-04-22 14:06:40 -0400

    Update ApiKeys.java

--- app/src/main/java/com/example/musicmoji/ApiKeys.java
+++ app/src/main/java/com/example/musicmoji/ApiKeys.java
@@ -7,4 +7,6 @@ public final class ApiKeys {
     public static final String spotifyClientID = "";
     
     public static final String ibmKey = "";
+    
+    public static final String ibmInstanceUrl = "";
 }
